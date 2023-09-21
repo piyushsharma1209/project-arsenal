13d9c36f04685f0fca44eadf4424149f823ff4be
piyushsharma1209 <piyush.sharma@stud.noroff.no>
2023-09-21 10:56:23 +0200

    new updates

--- script.js
+++ script.js
@@ -88,12 +88,4 @@ function toggleTableDisplay() {
     }
 }
 
-// Call the fetchStandings function to populate the table
 fetchStandings();
-
-
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function () {
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-}
